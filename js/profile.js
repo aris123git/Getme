cbae6124b58920e41306ae53ac091acffe312e70
aris123git <sawadogoaris226@gmail.com>
2026-05-28 23:59:11 +0000

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,4 @@
-import { supabase } from './main.js';
+import { supabase } from './supabaseClient.js';
 import { appState, updateBalance } from './state.js';
 import { api } from './api.js';
 import { UNLOCK_COST } from './config.js';
