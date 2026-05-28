3eb264fc169b8b8a04066993e62ce9c44d3f7e15
aris123git <sawadogoaris226@gmail.com>
2026-05-28 23:26:02 +0000

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,4 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
+import { https://nuijvjnufnaodwtrhjuq.supabase.co, sb_publishable_Z3l5W3VqqpSeFz6azxmrkw_fAZmT-iH } from './config.js';
+
+export const supabase = createClient(https://nuijvjnufnaodwtrhjuq.supabase.co, sb_publishable_Z3l5W3VqqpSeFz6azxmrkw_fAZmT-iH);
