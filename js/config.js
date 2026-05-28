1e0b6b3c721bdbc6ac7a7b51e8f05158d783222f
aris123git <sawadogoaris226@gmail.com>
2026-05-28 22:45:53 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,6 @@
-// Configuration Supabase
 export const SUPABASE_URL = 'https://nuijvjnufnaodwtrhjuq.supabase.co';
 export const SUPABASE_ANON_KEY = 'sb_publishable_Z3l5W3VqqpSeFz6azxmrkw_fAZmT-iH';
 
-// Constantes applicatives
 export const UNLOCK_COST = 500;
 export const DEFAULT_RADIUS = 5;
 export const MAX_RADIUS = 20;
