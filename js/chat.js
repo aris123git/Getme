05216154f4ca2352f9020f0cdc0af5c63c55767b
aris123git <sawadogoaris226@gmail.com>
2026-05-28 23:57:55 +0000

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,4 @@
-import { supabase } from './main.js';
+import { supabase } from './supabaseClient.js';
 import { appState, setState } from './state.js';
 import { api } from './api.js';
 import { escapeHtml, formatTime } from './utils.js';
