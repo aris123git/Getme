2d8c80ad71641e4684a2642dfe6d4eb632d02b9f
aris123git <sawadogoaris226@gmail.com>
2026-06-03 22:08:48 +0000

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
+import * as supabaseModule from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
 
-export const supabase = createClient(
+export const supabase = supabaseModule.createClient(
     'https://nuijvjnufnaodwtrhjuq.supabase.co',
     'sb_publishable_Z3l5W3VqqpSeFz6azxmrkw_fAZmT-iH'
 );
