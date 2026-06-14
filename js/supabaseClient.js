52e3e564a80dd6b1feebc7701a115c23989805b7
aris123git <sawadogoaris226@gmail.com>
2026-06-14 21:05:55 +0000

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js@2';
+
+export const supabase = createClient(
+    'https://nuijvjnufnaodwtrhjuq.supabase.co',
+    'sb_publishable_Z3l5W3VqqpSeFz6azxmrkw_fAZmT-iH'
+);
