a8a489ce868bf53611191165d1bfe7f0b0d86f54
aris123git <sawadogoaris226@gmail.com>
2026-06-14 21:08:19 +0000

    Add configuration constants and GPS options

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const UNLOCK_COST = 500;
+export const DEFAULT_RADIUS = 5;
+export const MAX_RADIUS = 20;
+export const GPS_OPTIONS = {
+    enableHighAccuracy: true,
+    maximumAge: 10000,
+    timeout: 10000
+};
